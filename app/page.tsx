dedb06fc83dc730cb1b211d9c72a57a96e63bb1d
Maxime PIGEON <maxpige@gmail.com>
2024-04-30 01:22:44 +0200

    improve wording

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { auth } from "@/edgedb";
 import Link from "next/link";
-import NextSteps from "@/components/NextSteps";
 
 export default async function Home() {
   const session = auth.getSession();
@@ -52,19 +51,21 @@ export default async function Home() {
         <div className="mx-auto max-w-2xl pt-16 sm:pt-24 lg:pt-32">
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              EdgeDB Next.js Starter
+              Clone Radar
             </h1>
             <p className="mt-6 text-base leading-7 text-gray-600">
-              Welcome to the EdgeDB Next.js Starter. This starter is designed to
-              help you get up and running with EdgeDB and Next.js quickly. It
-              includes a basic setup for authentication, EdgeDB schema, and a UI
-              to get you started. Below are some next steps to help you get up
-              to speed.
+              <div>You are unique in a lot of ways.</div>
+              <div>
+                One of them is the perfect combination of the things you love
+                now.
+              </div>
+              <div>
+                Pick the foundation of these things, meet the people who share
+                the same,
+              </div>
+              <div>and together, build something amazing on top.</div>
             </p>
           </div>
-        </div>
-        <div className="mx-auto max-w-2xl pt-4 sm:pt-8 lg:pt-12">
-          <NextSteps />
         </div>
       </div>
     </div>
