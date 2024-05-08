92ff8a5ff8d78af878c0bdf080ce38561e0e22c1
Maxime PIGEON <maxpige@gmail.com>
2024-05-08 16:40:07 +0200

    fix navbar active state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -51,7 +51,7 @@ export default function Navbar({
                       key={item.name}
                       href={item.href}
                       className={classNames(
-                        pathname === item.href
+                        `/${pathname.split("/")[1]}` === item.href
                           ? "border-primary text-gray-900"
                           : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
                         "inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium"
