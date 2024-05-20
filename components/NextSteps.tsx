c6bd948688593768be4eff18e89237c005cb5b90
Maxime PIGEON <maxpige@gmail.com>
2024-05-20 21:52:52 +0200

    fix next steps

--- components/NextSteps.tsx
+++ components/NextSteps.tsx
@@ -1,43 +1,36 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-unescaped-entities */
-import {
-  CircleStackIcon,
-  ListBulletIcon,
-  UserCircleIcon,
-} from "@heroicons/react/20/solid";
 
 const timeline = [
   {
     id: 0,
+    content: "Login",
+    description: <div>Login faster using your Github account</div>,
+  },
+  {
+    id: 1,
     content: "Collect your favorite YouTube channels",
     description: (
       <div>
         Paste URL from any channel, video or short to collect a new YouTube
-        Channel
+        channel
       </div>
     ),
-    icon: CircleStackIcon,
-  },
-  {
-    id: 1,
-    content: "Scan your clones on the radar",
-    description: (
-      <div>
-        You will add the 5 users with the most matching channels to your clone
-        list on each scan
-      </div>
-    ),
-    icon: UserCircleIcon,
   },
   {
     id: 2,
+    content: "Scan your clones on the radar",
+    description: <div>Find users with the most matching channels</div>,
+  },
+  {
+    id: 3,
     content: "Connect with your clones",
     description: (
       <div>
-        You can open a conversation with your clones even if they scanned you
+        You can open a conversation with your clones and visit their github
+        profiles
       </div>
     ),
-    icon: ListBulletIcon,
   },
 ];
 
@@ -45,7 +38,7 @@ export default function NextSteps() {
   return (
     <div className="py-10">
       <h2 className="text-center text-2xl mb-6 font-bold tracking-tight text-gray-900 sm:text-3xl">
-        How it works
+        Howto
       </h2>
       <div className="flow-root" suppressHydrationWarning>
         <ul role="list" className="-mb-8">
@@ -59,17 +52,14 @@ export default function NextSteps() {
                   />
                 ) : null}
                 <div className="relative flex space-x-3">
-                  <span className="bg-gray-700 h-10 w-10 rounded-full flex items-center justify-center ring-8 ring-white">
-                    <step.icon
-                      className="h-5 w-5 text-white"
-                      aria-hidden="true"
-                    />
+                  <span className="bg-gray-700 h-10 w-10 rounded-full flex items-center justify-center ring-8 ring-white text-white">
+                    {step.id + 1}
                   </span>
-                  <div className="flex flex-col min-w-0 flex-1 pt-2 w-96">
+                  <div className="flex flex-col min-w-0 flex-1 pt-2 w-72 items-start">
                     <p className="text-sm text-gray-700 font-semibold">
                       {step.content}
                     </p>
-                    <div className="mt-2 text-sm text-gray-500">
+                    <div className="mt-2 text-sm text-left text-gray-500">
                       {step.description}
                     </div>
                   </div>
