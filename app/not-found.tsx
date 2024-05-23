7deb5d7294ac84c3d5f57c9aa0c5a5641e30b929
Maxime PIGEON <maxpige@gmail.com>
2024-05-23 20:34:08 +0200

    fix youtube autoplay

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
         <iframe
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/sJO0n6kvPRU?autoplay=1&start=116&controls=0showinfo=0&modestbranding=0&rel=0"
+          src="https://www.youtube.com/embed/sJO0n6kvPRU?autoplay=1&start=116"
           title="YouTube video player"
           allow="autoplay;"
         ></iframe>
